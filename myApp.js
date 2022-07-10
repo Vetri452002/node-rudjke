bb20ec55bea84c3e89db720f4e0c89fc89fc10a2
Vetri452002 <vetrivelravichandran45@gmail.com>
2022-07-10 13:40:41 +0530

    2 files modified, 6 added, 1 deleted

---
+++ myApp.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let app = express();
+let absolutePath = __dirname + "/views/index.html";
+const mySecret = process.env['MESSAGE_STYLE']
+app.use("/public", express.static(__dirname + "/public"));
+//app.get("/", (req, res) => {
+  //res.send("Hello Express")
+//})
+
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath)
+})
+
+app.get("/json", (req, res) => {
+  if(mySecret ==="uppercase"){
+  res.json({"message":  "HELLO JSON"})
+  } else {
+    res.json({"message":"hello json"})
+  }
+})
+console.log("Hello World")
